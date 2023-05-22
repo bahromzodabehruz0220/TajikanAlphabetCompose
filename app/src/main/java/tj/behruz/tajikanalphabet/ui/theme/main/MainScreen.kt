f3fb2d0b568a32596349d6f10e9779824e7317cf
bahromzodabehruz0220 <122207192+bahromzodabehruz0220@users.noreply.github.com>
2023-05-21 18:20:47 -0700

    animation button

--- app/src/main/java/tj/behruz/tajikanalphabet/ui/theme/main/MainScreen.kt
+++ app/src/main/java/tj/behruz/tajikanalphabet/ui/theme/main/MainScreen.kt
@@ -1,11 +1,21 @@
 package tj.behruz.tajikanalphabet.ui.theme.main
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
@@ -13,11 +23,22 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import kotlinx.coroutines.delay
 import tj.behruz.tajikanalphabet.R
+import kotlin.time.Duration.Companion.seconds
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(navController: NavController) {
+    var visibleState by remember {
+        mutableStateOf(false)
+    }
+
+    LaunchedEffect(Unit) {
+        delay(1.seconds)
+        visibleState = true
+    }
+
     Box(
 
         modifier = Modifier
@@ -29,11 +50,21 @@ fun MainScreen(navController: NavController) {
         contentAlignment = Alignment.Center,
 
         ) {
-        Image(
-            modifier = Modifier.padding(top = 60.dp),
-            painter = painterResource(id = R.drawable.play_button),
-            contentDescription = "play_button"
-        )
+        AnimatedVisibility(
+            visible = visibleState,
+            modifier = Modifier
+                .fillMaxWidth(),
+            enter = slideInVertically() + fadeIn()
+        ) {
+            Image(
+                modifier = Modifier.padding(top = 60.dp).clickable {
+
+                },
+                painter = painterResource(id = R.drawable.play_button),
+                contentDescription = "play_button"
+            )
+        }
+
 
     }
 }
